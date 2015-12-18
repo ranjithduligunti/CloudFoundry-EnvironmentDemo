03986396d29b947525dbfdd1e8e2d11808534111
ranjithduligunti <ranjith.duligunti@citi.com>
2015-12-18 14:31:00 -0600

    Update Application.java

--- src/main/java/com/gopivotal/cf/workshop/Application.java
+++ src/main/java/com/gopivotal/cf/workshop/Application.java
@@ -10,7 +10,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Config.class, args);
-        System.out.println("Ranjith Testing");
+        System.out.println("Ranjith Testing1");
     }
     
 }
